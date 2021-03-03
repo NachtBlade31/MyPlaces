9a555427697cf1c609d903f5dcdb266945cfa5aa
NachtBlade31 <kbgoku@gmail.com>
2021-03-04 00:27:19 +0530

    connecting frontend and backed 101

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,15 @@ const userRoutes=require('./routes/users-routes');
 const HttpError=require("./models/http-error");
 const app=express();
 app.use(bodyParser.json());
+
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers',"Origin,X-Requested-With,Content-Type,Accept,Authorization");
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE');
+    next()
+});
+
 app.use('/api/places',placesRoutes);
 app.use('/api/users',userRoutes);
 // error handling route for all un existing routes
--- frontend/src/Modules/users/pages/Auth.js
+++ frontend/src/Modules/users/pages/Auth.js
@@ -6,11 +6,16 @@ import Card from '../../shared/components/UIElements/Card';
 import  {VALIDATOR_MINLENGTH,VALIDATOR_MAXLENGTH, VALIDATOR_EMAIL,VALIDATOR_REQUIRE} from '../../shared/utils/validators';
 import {useForm} from '../../shared/hooks/form-hook';
 import {AuthContext} from '../../shared/context/auth-context';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
 import './Auth.css';
 const Auth=props=>{
 
     const auth=useContext(AuthContext);
     const [isLoginMode,setIsLoginMode]=useState(true);
+    const [isLoading,setIsLoading]=useState(false);
+    const [error,setError]=useState();
 
     const [formState,inputHandler,setFormData]=useForm({
         email:{
@@ -44,15 +49,83 @@ const Auth=props=>{
         setIsLoginMode(prevMode=>!prevMode);
     }
 
-    const authSubmitHandler=event=>{
+    const authSubmitHandler=async event=>{
     event.preventDefault();
-    console.log(formState)
+     setIsLoading(true);
+
+    if(isLoginMode){
+        try{
+        const response=  await fetch('http://localhost:5000/api/users/login',{
+        method:'POST',
+       headers:{
+            'Content-Type':'application/json'
+       } ,
+       body:JSON.stringify({
+           email:formState.inputs.email.value,
+           password:formState.inputs.password.value
+       })
+    });
+    const responseData=await response.json();
+    if(!response.ok){
+        throw new Error(responseData.message);
+    }
+    setIsLoading(false);
     auth.login();
+
+        }
+        catch(err){
+           console.log(err) 
+           setIsLoading(false);
+
+           setError(err.message|| "Something went wrong! Please try again.");
+        }
+
+
+
+    }
+    else{
+        try{
+        const response=  await fetch('http://localhost:5000/api/users/signup',{
+        method:'POST',
+       headers:{
+            'Content-Type':'application/json'
+       } ,
+       body:JSON.stringify({
+           name:formState.inputs.name.value,
+           email:formState.inputs.email.value,
+           password:formState.inputs.password.value
+       })
+    });
+    const responseData=await response.json();
+    if(!response.ok){
+        throw new Error(responseData.message);
+    }
+
+    setIsLoading(false);
+    auth.login();
+
+        }
+        catch(err){
+           console.log(err) 
+           setIsLoading(false);
+
+           setError(err.message|| "Something went wrong! Please try again.");
+        }
+        
+    }
+    
+    
+}
+
+const errorHandler=()=>{
+    setError(null);
 }
 
     return(
-
-<Card className="authentication">
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler}/>
+        <Card className="authentication">
+    {isLoading &&<LoadingSpinner asOverlay/>}
     <h2>Login Required</h2>
     <hr/>
     <form onSubmit={authSubmitHandler}>
@@ -66,6 +139,8 @@ const Auth=props=>{
 
         <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode?'Signup':'Login'}</Button>
 </Card>
+
+        </React.Fragment>
     )
 }
 
